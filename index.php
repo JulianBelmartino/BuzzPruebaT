0da7fd774f60b19aec8b79a99c4832858a9e64c0
Julian Belmartino <belmartinojulian@gmail.com>
2024-01-31 18:58:35 -0300

    ejercicio php

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Log In</h1>
+    <form action="index.php" method="post">
+        <input type="text" name="text" /><br>
+        <input type="password" name="password" /><br>
+        <br>
+        <input type="submit" value="Log In" />
+    </form>
+</body>
+
+</html>
 <?php
-echo  "this is a fundational message";
+echo "Bienvenido {$_POST["text"]}";
+?>
+<p>fin</p>
